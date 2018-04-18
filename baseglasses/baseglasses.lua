b70d1038c3d29668b3de6758e9bcfe7c7956dab4
rater193 <rater193@gmail.com>
2018-04-17 20:10:14 -0400

    Added battery device support

--- baseglasses/baseglasses.lua
+++ baseglasses/baseglasses.lua
@@ -13,12 +13,22 @@ function createBatteryContent()
   local content = {
     ["bg"] = glasses.addRect(),
     ["textName"] = glasses.addTextLabel(),
-    ["textPower"] = glasses.addTextLabel()
+    ["textPower"] = glasses.addTextLabel(),
+    ["barBG"] = glasses.addRect(),
+    ["barFG"] = glasses.addRect()
   }
 
   content.bg.setColor(0,0,0)
   content.bg.setAlpha(0.5)
   content.bg.setSize(256,23)
+  
+  content.barBG.setColor(100,0,0)
+  content.barBG.setAlpha(0.5)
+  content.barBG.setSize(256,23)
+  
+  content.barFG.setColor(0,255,0)
+  content.barFG.setAlpha(1)
+  content.barFG.setSize(256,23)
 
   content.textName.setScale(1,1)
   content.textName.setColor(255,255,255)
@@ -48,7 +58,7 @@ tpsText.setText("TPS: ...")
 local batteries = {}
 
 for i, v in pairs(component.list()) do
-  if(v=="big_battery") then
+  if(v=="energy_device") then
     table.insert(batteries, {
       ["name"] = "N/A",
       ["power"] = {
@@ -72,14 +82,23 @@ thread.create(function()
       local bg = v.content.bg
       local textName = v.content.textName
       local textPower = v.content.textPower
+      local textStatus = v.content.textStatus
+      local barBG = v.content.barBG
+      local barFG = v.content.barFG
 
       local _y = 8+(i*36)
 
       bg.setPosition(8, _y)
       bg.setColor(0,0,0)
       bg.setAlpha(0.5)
-      bg.setSize(128,35)
+      bg.setSize(128,23)
 
+      barBG.setPosition(8, _y + 23)
+      barBG.setSize(128,12)
+
+      barFG.setPosition(8, _y + 23)
+      barFG.setSize(64,12)
+      
       textName.setPosition(10, _y+2)
       textName.setText("Battery: " .. tostring(v.name) .. "")
 
